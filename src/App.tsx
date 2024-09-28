a67ce871efa7066ae9e8ce8da09cdabe97ecb1cf
Vika <vikapetrova8@gmail.com>
2024-09-28 12:20:21 +0300

    delete all

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Counter} from "./components/Counter/Counter";
 
 function App() {
     return (
         <div className="App">
-            <Counter/>
         </div>
     );
 }
