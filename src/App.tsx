7f972ec778f32b5455f2d32cc6bf3d4de5fbb3aa
Vika <vikapetrova8@gmail.com>
2024-09-28 12:54:15 +0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let [value, setValue] = useState(0)
+
+    const maxValue = 5
+
+    const incOnClickHandler = () => {
+        if (value !== maxValue) {
+            setValue(value + 1)
+        } else {
+            setValue(maxValue)
+        }
+    }
+
+    const resetOnClickHandler = () => {
+        setValue(0)
+    }
+
     return (
         <div className="App">
+            <div className={'counter'}>
+                <div className={value !== maxValue ? 'block' : 'block error'}>{value}</div>
+                <div className={'btn-wrapper'}>
+                    <button disabled={value === maxValue} onClick={incOnClickHandler}>inc</button>
+                    <button disabled={value !== maxValue} onClick={resetOnClickHandler}>reset</button>
+                </div>
+            </div>
         </div>
     );
 }
