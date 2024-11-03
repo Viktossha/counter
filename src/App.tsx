20fa4397d0983e7e97afcda868f41c9411c19a51
Vika <vikapetrova8@gmail.com>
2024-11-03 20:57:36 +0300

    SecondAdvancedCounter component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {SimplyCounter} from "./SimplyCounter";
 import {AdvancedCounter} from "./AdvancedCounter";
+import {SecondAdvancedCounter} from "./SecondAdvancedCounter";
 
 function App() {
     return (
         <>
             {/*<SimplyCounter/>*/}
-            <AdvancedCounter/>
+            {/*<AdvancedCounter/>*/}
+            <SecondAdvancedCounter/>
         </>
     );
 }
---
+++ src/SecondAdvancedCounter.tsx
@@ -0,0 +1,89 @@
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import styles from './AdvancedCounter.module.css'
+import {Button} from "./Button";
+
+export const SecondAdvancedCounter = () => {
+    const [maxValue, setMaxValue] = useState(5)
+    const [startValue, setStartValue] = useState(0)
+    const [currentValue, setCurrentValue] = useState(startValue)
+    const [isFocused, setIsFocused] = useState(false)
+    const [showSettings, setShowSettings] = useState(false)
+
+    useEffect(() => {
+        let start = localStorage.getItem('startValue')
+        let max = localStorage.getItem('maxValue')
+        if (start && max) {
+            setStartValue(JSON.parse(start))
+            setMaxValue(JSON.parse(max))
+            setCurrentValue(JSON.parse(start))
+        }
+    }, []);
+
+    const onChangeMaxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setCurrentValue(0)
+        setIsFocused(true)
+        setMaxValue(+e.currentTarget.value)
+    }
+
+    const onChangeStartValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setCurrentValue(0)
+        setIsFocused(true)
+        setStartValue(+e.currentTarget.value)
+    }
+
+    const incOnClickHandler = () => {
+        if (currentValue < maxValue) {
+            setCurrentValue(currentValue + 1)
+        }
+    }
+
+    const resetOnClickHandler = () => {
+        setCurrentValue(startValue)
+    }
+
+    const setOnClickHandler = () => {
+        setIsFocused(false)
+        setCurrentValue(startValue)
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        setShowSettings(false)
+    }
+
+    const showSettingsHandler = () => {
+        setShowSettings(true)
+    }
+
+    const incorrectValue = maxValue < 0 || startValue < 0 || maxValue < startValue || maxValue === startValue
+    const text = incorrectValue ? 'Incorrect value' : 'Enter values and press "set"'
+
+    return (
+        <div className={styles.wrapper}>
+
+            {showSettings && <div className={styles.settings}>
+                <div className={styles.inputs}>
+                    <label>max value:
+                        <input type="number" name="maxValue" value={maxValue} onChange={onChangeMaxValueHandler}/>
+                    </label>
+                    <label>start value:
+                        <input type="number" name="startValue" value={startValue} onChange={onChangeStartValueHandler}/>
+                    </label>
+                </div>
+                <div className={styles.btnBlock}>
+                    <Button title={'set'} onClick={setOnClickHandler} disabled={!isFocused || incorrectValue}/>
+                </div>
+            </div>}
+
+
+            <div className={styles.counter}>
+                <div
+                    className={maxValue === currentValue ? styles.block + ' ' + styles.error : (isFocused ? styles.block + ' ' + styles.blockText : styles.block)}>{isFocused ? text : currentValue}</div>
+                <div className={styles.btnWrapper}>
+                    <Button title={'inc'} onClick={incOnClickHandler} disabled={maxValue === currentValue}/>
+                    <Button title={'reset'} onClick={resetOnClickHandler}/>
+                    <Button title={'set'} onClick={showSettingsHandler}/>
+                </div>
+            </div>
+
+        </div>
+    );
+};
